f5229da7dc7f487e97e32f8d40f7686099b1e4ff
John Magee <jemagee@cox.net>
2017-05-18 19:23:31 -0700

    Setting up and testing some basic validations on fields

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,5 @@
 class Vendor < ApplicationRecord
+
+	validates :name, presence: true, uniqueness: {case_sensitive: false}
+	validates :contact_email, uniqueness: {case_sensitive: false}
 end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should validate_presence_of(:name)}
+  it {should validate_uniqueness_of(:name).case_insensitive}
+  it {should validate_uniqueness_of(:contact_email).case_insensitive}
 end
