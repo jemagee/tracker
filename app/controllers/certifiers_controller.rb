aca986e24c5fc9c79f8c5e116e737dab8916bc02
John Magee <jemagee@cox.net>
2017-05-25 18:39:30 -0700

    Finishing up the requiredments to properly test certifier creation basics and testing

--- app/controllers/certifiers_controller.rb
+++ app/controllers/certifiers_controller.rb
@@ -3,4 +3,21 @@ class CertifiersController < ApplicationController
 	def new
 		@certifier = Certifier.new
 	end
+
+	def create
+		@certifier = Certifier.new(certifier_params)
+		@certifier.save
+		flash[:success] = "Certifier Successfully Created!"
+		redirect_to certifier_path(@certifier)
+	end
+
+	def show
+		@certifier = Certifier.find(params[:id])
+	end
+
+	private
+
+		def certifier_params
+			params.require(:certifier).permit(:organization, :certification_id)
+		end
 end
--- spec/features/creating_certifier_spec.rb
+++ spec/features/creating_certifier_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Creating a certifier" do
 
 	5.times do |n|
-		let("certification#{n}") {FactoryGirl.create(:certification)}
+		let!("certification#{n}") {FactoryGirl.create(:certification)}
 	end
 
 	scenario "works properly" do
@@ -14,6 +14,12 @@ RSpec.feature "Creating a certifier" do
 		click_button "Add Certifier"
 
 		expect(page).to have_content("Certifier Successfully Created!")
+		within("header") do
+			expect(page).to have_content("New Certifier")
+		end
+		within("div#certification") do
+			expect(page).to have_content certification2.name
+		end
 
 	end
 end
